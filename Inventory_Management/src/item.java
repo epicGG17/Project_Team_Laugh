24a6390ae0f0dbed4e99ddcf589cd9cd3d62fcc4
Bryan <Bryan@DESKTOP-1APKKAG>
2020-11-14 09:28:46 -0800

    Linked list version of the program

--- Inventory_Management/src/item.java
+++ Inventory_Management/src/item.java
@@ -1,57 +1,118 @@
-
-public class item {
+//linked list, with methods to interact with the nodes
+public class item {	
+	
 	//attributes
-	private String category;
-	private double price;
-	private int id;
-	private int quantity;
+	private node first;
+	private node last;
+	private int size;
 	
-	//constructors
+	//Constructors
 	public item() {
-		category = null;
-		price = 0.0;
-		id = 0;
-		quantity = 0;
+		first = null;
+		last = first;
 	}
 	
-	public item(String s, double b, int i, int q) {
-		category = s;
-		price = b;
-		id = i;
-		quantity = q;
+	//check if empty
+	public boolean isEmpty() {
+		if(first == null)
+			return true;
+		else
+			return false;
 	}
 	
-	//Get attribute methods
-	public String getCategory() {
-		return category;
+	//get first node
+	public node getFirst() {
+		return first;
 	}
 	
-	public double getPrice() {
-		return price;
+	//get last
+	public node getLast() {
+		return last;
 	}
 	
-	public int getID() {
-		return id;
+	//get number total number of different items in list
+	public int getSize() {
+		return size;
 	}
 	
-	public int getQuantity() {
-		return quantity;
+	//add a new item in linked list
+	public void addItem(String s, String n,double b,int i, int q) {
+		if(isEmpty()) {
+			first = new node(s,n,b,i,q);
+			last = first;
+			size++;
+		}
+		else {
+			node add = new node(s,n,b,i,q);
+			last.setLink(add);
+			last = last.getLink();
+			size++;
+		}
 	}
 	
-	//set attribute methods
-	public void setCategory(String s) {
-		category = s;
+	public void addItem(node n) {
+		if(isEmpty()) {
+			first = n;
+			last = first;
+			size++;
+		}
+		else {
+			node add = n;
+			last.setLink(add);
+			last = last.getLink();
+			size++;
+		}
 	}
 	
-	public void setPrice(double p) {
-		price = p;
+	//search an id number and delete an object with matching id num
+	public void delete(int idNum) {
+		boolean found = false;
+		if(first.getID() == idNum) {
+			first = null;
+			found = true;
+		}
+		else {
+			node trail = first;
+			node temp = first.getLink();
+			while(temp!=null) {
+				if(temp.getID() == idNum) {
+					trail.setLink(temp.getLink());
+					temp = null;
+					found = true;
+					break;
+				}
+				else {
+					temp = temp.getLink();
+					trail = trail.getLink();
+				}
+			}
+		}
+		if(found) {
+			System.out.println("Item with id number " + idNum + " has been deleted");
+			size--;
+		}
+		else {
+			System.out.println("Item with id number " + idNum + " not found");
+		}
 	}
 	
-	public void setID(int i) {
-		id = i;
-	}
-	
-	public void setQuantity(int q) {
-		quantity = q;
+	//print all info in each node of the list
+	public void print() {
+		node temp = first;
+		System.out.println("Number of items in inventory: " + size);
+		if(size == 0) {
+			System.out.print("List is empty");
+		}
+		else {
+			while(temp!= null) {
+				System.out.print("Category: " + temp.getCategory() + ", ");
+				System.out.print("Name: " + temp.getName() + ", ");
+				System.out.print("Price: " + temp.getPrice() + ", ");
+				System.out.print("ID: " + temp.getID() + ", ");
+				System.out.print("Quantity: " + temp.getQuantity() + "\n");
+				
+				temp = temp.getLink();
+			}		
+		}		
 	}
 }
--- Inventory_Management/src/methodTest.java
+++ Inventory_Management/src/methodTest.java
@@ -5,136 +5,106 @@ import org.junit.Test;
 @SuppressWarnings("unused")
 public class methodTest {
 	//testing get methods
+
 	@Test
-    public void getCategory(){
+    public void testAdd(){
         String c = "laptop";
+        String n = "Sasmsung";
         double p = 10.9;
         int i = 1234;
         int q = 4;
         
-        item obj = new item(c,p,i,q);
+        node testNode = new node(c, n, p, i, q);
+        
+        item arr = new item();
+        arr.addItem(c, n, p, i, q);
+        arr.addItem(c, n, p+1, i+1, q);
 
-
-      assertEquals("getCategory()",c,obj.getCategory());
+        assertEquals("test add method", testNode.getID()+1,arr.getLast().getID());
     }
 	
 	@Test
-    public void getPrice(){
+    public void testAdd2(){
         String c = "laptop";
+        String n = "Sasmsung";
         double p = 10.9;
         int i = 1234;
         int q = 4;
         
-        item obj = new item(c,p,i,q);
+        node testNode = new node(c, n, p, i, q);
+        
+        item arr = new item();
+        arr.addItem(c, n, p, i, q);
+        arr.addItem(c, n, p+1, i+1, q);
 
-
-      assertEquals("getPrice()",p,obj.getPrice(),.01);
+        assertEquals("test add method", testNode.getID(),arr.getFirst().getID());
     }
 	
 	@Test
-    public void getID(){
+    public void testDelete(){
         String c = "laptop";
+        String n = "Sasmsung";
         double p = 10.9;
         int i = 1234;
         int q = 4;
+        int count = 3;
+        int idNum = 1234;
         
-        item obj = new item(c,p,i,q);
+        node testNode = new node(c, n, p, i, q);
+        node testNode2 = new node(c, n, p+1, i+1, q+1);
+        node testNode3 = new node(c, n, p+2, i+2, q+2);
+        
+        item arr = new item();
+        
+        arr.addItem(testNode);
+        arr.addItem(testNode2);
+        arr.addItem(testNode3);
+        
+        arr.delete(1235);
 
-
-      assertEquals("getID()",i,obj.getID());
+        assertEquals("test delete method", count-1 ,arr.getSize());
     }
 	
 	@Test
-    public void getQuantity(){
+    public void testDelete2(){
         String c = "laptop";
+        String n = "Sasmsung";
+        double p = 10.9;
+        int i = 1234;
+        int q = 4;
+        int count = 3;
+        int idNum = 1234;
+        
+        node testNode = new node(c, n, p, i, q);
+        node testNode2 = new node(c, n, p+1, i+1, q+1);
+        node testNode3 = new node(c, n, p+2, i+2, q+2);
+        
+        item arr = new item();
+        
+        arr.addItem(testNode);
+        arr.addItem(testNode2);
+        arr.addItem(testNode3);
+        
+        arr.delete(1245);
+
+        assertEquals("test delete method", count ,arr.getSize());
+    }
+
+	@Test
+    public void testprint(){
+        String c = "laptop";
+        String n = "Sasmsung";
         double p = 10.9;
         int i = 1234;
         int q = 4;
         
-        item obj = new item(c,p,i,q);
+        node testNode = new node(c, n, p, i, q);
+        
+        item arr = new item();
+        arr.addItem(c, n, p, i, q);
+        arr.addItem(c, n, p+1, i+1, q);
 
-
-      assertEquals("getQuantity()",q,obj.getQuantity());
+        arr.print();
     }
 	
-	//test set methods
-	@Test
-    public void setCategory(){
-        String c = "laptop";
-        double p = 10.9;
-        int i = 1234;
-        int q = 4;
-        
-        item obj = new item(c,p,i,q);
-        
-        String c2 = "TV";
-        double p2 = 15.4;
-        int i2 = 4413;
-        int q2 = 7;
-        
-        obj.setCategory(c2);
-
-
-      assertEquals("get altered Category",c2,obj.getCategory());
-    }
-	
-	@Test
-    public void setPrice(){
-        String c = "laptop";
-        double p = 10.9;
-        int i = 1234;
-        int q = 4;
-        
-        item obj = new item(c,p,i,q);
-        
-        String c2 = "TV";
-        double p2 = 15.4;
-        int i2 = 4413;
-        int q2 = 7;
-        
-        obj.setPrice(p2);
-
-
-      assertEquals("get altered price",p2,obj.getPrice(),.001);
-    }
-	
-	@Test
-    public void setID(){
-        String c = "laptop";
-        double p = 10.9;
-        int i = 1234;
-        int q = 4;
-        
-        item obj = new item(c,p,i,q);
-        
-        String c2 = "TV";
-        double p2 = 15.4;
-        int i2 = 4413;
-        int q2 = 7;
-        
-        obj.setID(i2);
-
-
-      assertEquals("get altered id",i2,obj.getID());
-    }
-	
-	@Test
-    public void setQuantity(){
-        String c = "laptop";
-        double p = 10.9;
-        int i = 1234;
-        int q = 4;
-        
-        item obj = new item(c,p,i,q);
-        
-        String c2 = "TV";
-        double p2 = 15.4;
-        int i2 = 4413;
-        int q2 = 7;
-        
-        obj.setQuantity(q2);
-
-
-      assertEquals("get altered quantity",q2,obj.getQuantity());
-    }
 }
