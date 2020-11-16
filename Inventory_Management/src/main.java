04a62ef1dae2508c9e3b1f591b9e722410828fe1
DESKTOP-57MAGT4\joseph <joseohac83@gmail.com>
2020-11-15 20:41:18 -0800

    reads from file to the linked list writes to a second file and asks user if they want to save

---
+++ Inventory_Management/src/main.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class main {
+	
+	public static void main(String[] args) throws IOException {
+		//creates linked list
+		item arr = new item();
+		file.scanFile(arr);
+		
+		//test write file
+	    arr.addItem("Console", "Xbox-One", 499.99, 9, 22);
+		arr.addItem("Console", "Wii", 149.99, 9, 22);
+		file.writeFile(arr);
+		
+		//as
+		askSave(arr);
+		
+	}
+	
+	// asks if the user wants to save the file 
+	static void askSave(item arr) throws IOException {
+		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		System.out.println("Enter yes/YES to save");
+		String s = myObj.nextLine().toUpperCase();  // Read user input
+		
+		switch(s) {
+		  case "YES":
+			file.writeFile(arr);
+			  file.saveFile(arr);
+			  System.out.println("File was saved.");
+		      break;
+		  default:
+			  System.out.println("File was not saved.");
+		}
+		
+	}
+
+}
